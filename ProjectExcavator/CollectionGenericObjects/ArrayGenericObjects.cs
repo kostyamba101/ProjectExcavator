f027fee3cdbab08ae161efb165b08b8f7784ca6a
kostyamba101 <kostyamba101@gmail.com>
2024-10-14 13:33:08 +0400

    Добавление коллекции

---
+++ ProjectExcavator/CollectionGenericObjects/ArrayGenericObjects.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectExcavator.CollectionGenericObjects;
+/// <summary>
+/// Параметризованный набор объектов
+/// </summary>
+/// <typeparam name="T">Параметр: ограничение - ссылочный тип</typeparam>
+public class ArrayGenericObjects<T> : ICollectionGenericObjects<T>
+    where T : class
+{
+    /// <summary>
+    /// Массив объектов, которые храним
+    /// </summary>
+    private T?[] _collection;
+    public int Count => _collection.Length;
+
+    public int SetMaxCount { set { if (value > 0) { _collection = new T?[value]; } } }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public ArrayGenericObjects()
+    {
+        _collection = Array.Empty<T>();
+    }
+
+    public T? Get(int position)
+    {
+        if (position < 0 || position >= _collection.Length)
+        {
+            return null;
+        }
+        return _collection[position];
+    }
+
+    public bool Insert(T obj)
+    {
+        for (int i = 0; i < _collection.Length; i++)
+        {
+            if (_collection[i] == null)
+            {
+                _collection[i] = obj;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool Insert(T obj, int position)
+    {
+        if (position < 0 || position >= _collection.Length)
+        {
+            return false;
+        }
+
+        // Если позиция свободна, вставляем объект
+        if (_collection[position] == null)
+        {
+            _collection[position] = obj;
+            return true;
+        }
+
+        // Поиск ближайшего свободного места после позиции
+        for (int i = position + 1; i < _collection.Length; i++)
+        {
+            if (_collection[i] == null)
+            {
+                _collection[i] = obj;
+                return true;
+            }
+        }
+
+        // Поиск ближайшего свободного места до позиции
+        for (int i = position - 1; i >= 0; i--)
+        {
+            if (_collection[i] == null)
+            {
+                _collection[i] = obj;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool Remove(int position)
+    {
+        if (position < 0 || position >= _collection.Length)
+        {
+            return false;
+        }
+
+        if (_collection[position] != null)
+        {
+            _collection[position] = null;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ ProjectExcavator/CollectionGenericObjects/ICollectionGenericObjects.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectExcavator.CollectionGenericObjects;
+/// <summary>
+/// Интерфейс описания действий для набора хранимых объектов
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface ICollectionGenericObjects<T>
+    where T : class
+{
+    /// <summary>
+    /// Количество объектов в коллекции
+    /// </summary>
+    int Count { get; }
+    /// <summary>
+    /// Установка максимального количества элементов
+    /// </summary>
+    int SetMaxCount { set; }
+    /// <summary>
+    /// Добавление объекта в коллекцию
+    /// </summary>
+    /// <param name="obj">Добавляемый объект</param>
+    /// <returns>true-вставка прошла успешно, false - вставка не удалась</returns>
+    bool Insert(T obj);
+    /// <summary>
+    /// Добавление объекта в коллекцию на конкретную позицию
+    /// </summary>
+    /// <param name="ojb">Добавляемый объект</param>
+    /// <param name="position">Позиция</param>
+    /// <returns>true-вставка прошла успешно, false - вставка не удалась</returns>
+    bool Insert(T ojb, int position);
+    /// <summary>
+    /// Удаление объекта из коллекции с конкретной позиции
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>true-удаление прошло успешно, false - удаление не удалась</returns>
+    bool Remove(int position);
+    /// <summary>
+    /// Получение объекта по позиции
+    /// </summary>
+    /// <param name="position">Позиция</param>
+    /// <returns>Объект</returns>
+    T? Get(int position);
+}
